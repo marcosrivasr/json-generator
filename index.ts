a5cc1937499e29b55e53ecd26b1986cbf4d17b7f
Marcos Rivas Rojas <marcosrivasr@gmail.com>
2020-07-09 15:44:37 -0500

    validation of entries

--- index.ts
+++ index.ts
@@ -1,33 +1,50 @@
 import {readFileSync, readFile} from 'fs';
 import { v4 as uuidv4 } from 'uuid';
-import generateRandomName from './names';
+import {generateRandomFirstName} from './types/names';
+import {generateRandomLastName} from './types/names';
+import generateRandomAge from './types/age';
+import generateRandomNumber from './types/number';
+import generateRandomPhone from './types/phone';
 
 const json = JSON.parse(readFileSync('template.json', 'utf8'));
 
 
 parse(json);
 
+
 function parse(json:any){
     const keys:string[] = Object.keys(json);
-    let finalJSON:any = {};
+
+    const regexs = [/uuid/, 
+                    /^number\((-?[0-9]+,-?[0-9]+)?\)$/, 
+                    /^generateRandomName\(\)$/, 
+                    /^generateRandomAge\(([1-9]+,[1-9]+)?\)$/
+                ];
 
     keys.forEach(key => {
         const type = json[key];
         if(typeof type === 'string'){
-            switch(type){
+
+             switch(type){
                 case 'uuid':
-                    finalJSON['sdsd'] = 'sdsd'
-                    console.log(finalJSON);
+                    console.log(key, uuidv4());
                 break;
                 case 'number':
-                    console.log(key, Math.floor(Math.random() * 100));
+                    console.log(key, generateRandomNumber());
                 break;
-                case 'name':
-                    console.log(generateRandomName());
-                    finalJSON += JSON.stringify({key: generateRandomName()});
-                    console.log(finalJSON);
+                case 'first-name':
+                    console.log(key, generateRandomFirstName());
                 break;
-            }
+                case 'last-name':
+                    console.log(key, generateRandomLastName());
+                break;
+                case 'age':
+                    console.log(key, generateRandomAge());
+                break;
+                case 'phone':
+                    console.log(key, generateRandomPhone());
+                break;
+            } 
         }else if(typeof type === 'object'){
             const arr = type;
             const settings = arr[0];
--- names.ts
+++
@@ -1,9 +0,0 @@
-import { readFileSync } from 'fs';
-
-export default function generateRandomName(){
-    const file = './data/names.json';
-    
-    const content = readFileSync(file, 'utf-8');
-    const arr = JSON.parse(content);
-    return arr[Math.floor(Math.random() * arr.length)];
-}
