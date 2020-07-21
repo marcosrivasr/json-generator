de59f8e382897e93d086a6c48c64f0d16971ecca
Marcos Rivas Rojas <marcosrivasr@gmail.com>
2020-07-21 11:14:07 -0500

    parse() function modified to build the JSON string

--- index.ts
+++ index.ts
@@ -1,60 +1,87 @@
-import {readFileSync, readFile} from 'fs';
+import {readFileSync, readFile,writeFileSync} from 'fs';
 import { v4 as uuidv4 } from 'uuid';
-import {generateRandomFirstName} from './types/names';
-import {generateRandomLastName} from './types/names';
+import {generateRandomFirstName, generateRandomLastName} from './types/names';
 import generateRandomAge from './types/age';
-import generateRandomNumber from './types/number';
+import {generateRandomNumber, generateRandomDecimal, generateRandomPrice} from './types/number';
 import generateRandomPhone from './types/phone';
+import generateRandomCreditCardNumber from './types/creditcard';
 
 const json = JSON.parse(readFileSync('template.json', 'utf8'));
 
+let res = [];
+res = parse(json);
 
-parse(json);
+const finalJSON = Object.fromEntries(res);
+
+writeFileSync('./output.json', JSON.stringify(finalJSON,null,"\t"), "UTF-8");
 
 
-function parse(json:any){
+
+
+function parse(json:any):[string, string|number][]{
     const keys:string[] = Object.keys(json);
+    let res:[string, any][] = [];
 
-    const regexs = [/uuid/, 
-                    /^number\((-?[0-9]+,-?[0-9]+)?\)$/, 
-                    /^generateRandomName\(\)$/, 
-                    /^generateRandomAge\(([1-9]+,[1-9]+)?\)$/
-                ];
-
+    
+    
     keys.forEach(key => {
         const type = json[key];
+        
         if(typeof type === 'string'){
 
              switch(type){
                 case 'uuid':
-                    console.log(key, uuidv4());
+                    //console.log(key, uuidv4());
+                    res.push([key, uuidv4()]);
                 break;
                 case 'number':
-                    console.log(key, generateRandomNumber());
+                    //console.log(key, generateRandomNumber());
+                    res.push([key, generateRandomNumber()]);
                 break;
                 case 'first-name':
-                    console.log(key, generateRandomFirstName());
+                    //console.log(key, generateRandomFirstName());
+                    res.push([key, generateRandomFirstName()]);
                 break;
                 case 'last-name':
-                    console.log(key, generateRandomLastName());
+                    //console.log(key, generateRandomLastName());
+                    res.push([key, generateRandomLastName()]);
                 break;
                 case 'age':
-                    console.log(key, generateRandomAge());
+                    //console.log(key, generateRandomAge());
+                    res.push([key, generateRandomAge()]);
                 break;
                 case 'phone':
-                    console.log(key, generateRandomPhone());
+                    //console.log(key, generateRandomPhone());
+                    res.push([key, generateRandomPhone()]);
                 break;
+                case 'decimal':
+                    //console.log(key, generateRandomDecimal());
+                    res.push([key, generateRandomDecimal()]);
+                break;
+                case 'price':
+                    //console.log(key, generateRandomPrice());
+                    res.push([key, generateRandomPrice()]);
+                break;
+                case 'credit-card':
+                    //console.log(key, generateRandomCreditCardNumber());
+                    res.push([key, generateRandomCreditCardNumber()]);
+                break;
+                default:
             } 
         }else if(typeof type === 'object'){
             const arr = type;
             const settings = arr[0];
             const obj = arr[1];
             const repeat = settings['repeat'];
-            for(let i=1; i< repeat; i++){
-                parse(obj);
+            let temp = [];
+            for(let i = 1; i <= repeat; i++){
+
+                temp.push(Object.fromEntries(parse(obj)));
             }
+            res.push([key, temp]);
         }
     });
+    return res;
 }
 
 
