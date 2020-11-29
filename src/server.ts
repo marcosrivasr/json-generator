93fb0cf8d98978353abdf09b50e3ca02d846fde5
Marcos Rivas Rojas <marcosrivasr@gmail.com>
2020-11-29 16:21:17 -0600

    remove comments

--- src/server.ts
+++ src/server.ts
@@ -2,78 +2,12 @@ import express from 'express';
 import {join, basename} from 'path';
 import { readFile } from 'fs/promises';
 import cors from 'cors';
-import { callbackify } from 'util';
-
-/* export default class Server{
-
-    private app;
-
-    constructor(callback:any){
-        this.app = express();
-
-        this.app.use(cors());
-
-        this.app.get('/:name', async (req, res) => {
-            const filename = req.params.name;
-            const api = join(__dirname, 'apis', filename + '.json');
-        
-            const json = await readFile(api, 'utf-8');
-            console.log(JSON.parse(json));
-            
-            res.json(JSON.parse(json));
-        });
-        
-        this.app.get('/:filename/:object/:id', async (req, res) => {
-            const filename:string = req.params.filename;
-            const object:string = req.params.object;
-            const id:string = req.params.id;
-            const api = join(__dirname, 'apis', filename + '.json');
-            try{
-                const file = await readFile(api, 'utf-8');
-                const json = JSON.parse(file);
-                
-                const array = <any[]>json[object];
-                const resultado = array.filter(x => x.id == id);
-                if(resultado.length > 0){
-                    res.json(resultado[0]);
-                }else{
-                    res.status(404).json({error: 'no results from the query'});
-                }
-            }catch(exception){
-                res.status(404).json({error: 'no results from the query'});
-            }
-        });
-        
-        this.app.get('/:filename/:object/', async (req, res) => {
-            const filename:string = req.params.filename;
-            const object:string = req.params.object;
-            const api = join(__dirname, 'apis', filename + '.json');
-        
-            try{
-                const file = await readFile(api, 'utf-8');
-                const json = JSON.parse(file);
-                
-                res.status(200).json(json);
-            }catch(exception){
-                res.status(404).json({error: 'no results from the query'});
-            }
-        });
-        
-        this.app.get('/', (req, res) => {
-            res.send('HOLA');
-        });
-        
-        this.app.listen(3333, callback);
-    }
-
-
-} */
 
 const app = express();
 
 app.use(cors());
 
- app.get('/:name', async (req, res) => {
+app.get('/:name', async (req, res) => {
     const filename = req.params.name;
     const api = join(__dirname, 'apis', filename + '.json');
 
@@ -120,6 +54,7 @@ app.get('/:filename/:object/', async (req, res) => {
 });
 
 app.get('/', (req, res) => {
+    
     res.send('HOLA');
 });
 
