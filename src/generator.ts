3392cf5fd990082df19e73d366f0b0c93f66a9f5
Marcos Rivas Rojas <marcosrivasr@gmail.com>
2021-01-17 21:40:42 -0600

    disaster

--- src/generator.ts
+++ src/generator.ts
@@ -1,15 +1,19 @@
 import {readFileSync, readFile,writeFileSync} from 'fs';
+import { array } from 'yargs';
 import TypeFactory from './typeFactory';
 
 export default class Generator{
 
     private factory?:TypeFactory;
-    private output:[string, string|number][]=[];
+    private output:any;
     private filecontent:string = '';
 
     constructor(filename?:string){
+        //new factory instance
         this.factory = new TypeFactory();
+
         if(filename !== undefined){
+            //if filename exists, read and return the text of the file
             this.filecontent = JSON.parse(readFileSync(filename, 'utf8'));
         }
     }
@@ -18,7 +22,12 @@ export default class Generator{
      * @param filename Filename for the output file
      */
     generateJSON(filename:string){
-        this.output = this.parse(this.filecontent, this.factory!);
+        let output = null;
+        if(Array.isArray(this.filecontent)){
+            output = this.parse(this.filecontent, this.factory!);
+        }else{
+            output = this.parse(this.filecontent, this.factory!);
+        }
 
         const finalJSON = Object.fromEntries(this.output);
 
@@ -26,7 +35,27 @@ export default class Generator{
     }
 
     createJSON(text:string):string{
-        const output = this.parse(text, this.factory!);
+        console.log('Start parse process...');
+        let output = null;
+        if(Array.isArray(text)){
+            console.log('************************** El JSON es un arreglo inicial');
+            const objSettings   = text[0];
+            const template      = text[1];
+            const repeat        = objSettings['repeat'];
+            let temp = [];
+
+            for(let i = 1; i <= repeat; i++){
+                const item = new Map<string, any>([this.parse(template, this.factory!)]);
+                console.log('0.', item);
+                const object = Object.fromEntries(item);
+                temp.push(object);
+            }
+            output = [...temp];
+        }else{
+            console.log('************************** El JSON es un objeto inicial');
+            output = this.parse(text, this.factory!);
+        }
+        
         const finalJSON = Object.fromEntries(output);
         return JSON.stringify(finalJSON,null, '\t');
     }
@@ -36,7 +65,7 @@ export default class Generator{
      * @param json JSON text
      * @param factory 
      */
-    private parse(json:any, factory:TypeFactory):[string, string|number][]{
+    private parse(json:any, factory:TypeFactory){
         const keys:string[] = Object.keys(json);
         let res:[string, any][] = [];
         
@@ -51,22 +80,11 @@ export default class Generator{
                 let subfactory = new TypeFactory();
                 if(!Array.isArray(type)){
                     res.push([key, Object.fromEntries(this.parse(subobj, subfactory))]);
-                }else{
-                    const settings = subobj[0];
-                    const obj = subobj[1];
-                    const repeat = settings['repeat'];
-                    let temp = [];
-                    
-                    for(let i = 1; i <= repeat; i++){
-
-                        temp.push(Object.fromEntries(this.parse(obj, subfactory)));
-                    }
-                    res.push([key, temp]);
+                
                 }
                 
             }
         });
-
-        return res;
     }
+
 }
